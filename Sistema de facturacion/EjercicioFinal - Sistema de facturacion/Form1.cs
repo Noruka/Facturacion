3c1cae4750a87f64410ae9de7f8d92318ecdf097
Noruka <44400784+Noruka@users.noreply.github.com>
2018-10-29 18:08:50 +0100

    Update Form1.cs

--- Sistema de facturacion/EjercicioFinal - Sistema de facturacion/Form1.cs
+++ Sistema de facturacion/EjercicioFinal - Sistema de facturacion/Form1.cs
@@ -16,5 +16,10 @@ namespace EjercicioFinal___Sistema_de_facturacion
         {
             InitializeComponent();
         }
+        
+        private void btnAcceder_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
